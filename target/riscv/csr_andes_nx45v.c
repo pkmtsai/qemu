8119b2953c790dd4dd777fe945a0c8e9cb53b90a
Yu-Ming Chang <yumin686@andestech.com>
2023-08-15 17:30:51 +0800

    target/riscv: Fix compiling overflow error when compile with rv32               targets.
    Signed-off-by: Yu-Ming Chang <yumin686@andestech.com>

--- target/riscv/csr_andes_nx45v.c
+++ target/riscv/csr_andes_nx45v.c
@@ -10,35 +10,36 @@
 static RISCVException read_mmsc_cfg_nx45v(CPURISCVState *env, int csrno,
                                           target_ulong *val)
 {
-    static target_ulong mask = MASK_MMSC_CFG_ECC     | MASK_MMSC_CFG_ECD
-                             | MASK_MMSC_CFG_PFT     | MASK_MMSC_CFG_HSP
-                             | MASK_MMSC_CFG_ACE     | MASK_MMSC_CFG_VPLIC
-                             | MASK_MMSC_CFG_EV5PE   | MASK_MMSC_CFG_LMSLVP
-                             | MASK_MMSC_CFG_PMNDS   | MASK_MMSC_CFG_CCTLCSR
-                             | MASK_MMSC_CFG_VCCTL   | MASK_MMSC_CFG_NOPMC
-                             | MASK_MMSC_CFG_EDSP    | MASK_MMSC_CFG_PPMA
-                             | MASK_MMSC_CFG_BF16CVT | MASK_MMSC_CFG_ZFH
-                             | MASK_MMSC_CFG_VL4     | MASK_MMSC_CFG_VECCFG
-                             | MASK_MMSC_CFG_VSIH    | MASK_MMSC_CFG_VDOT
-                             | MASK_MMSC_CFG_VPFH    | MASK_MMSC_CFG_L2CMP_CFG
-                             | MASK_MMSC_CFG_L2C     | MASK_MMSC_CFG_IOCP
-                             | MASK_MMSC_CFG_CORE_PCLUS
-                             | MASK_MMSC_CFG_RVARCH;
+    static uint64_t mask = MASK_MMSC_CFG_ECC     | MASK_MMSC_CFG_ECD
+                         | MASK_MMSC_CFG_PFT     | MASK_MMSC_CFG_HSP
+                         | MASK_MMSC_CFG_ACE     | MASK_MMSC_CFG_VPLIC
+                         | MASK_MMSC_CFG_EV5PE   | MASK_MMSC_CFG_LMSLVP
+                         | MASK_MMSC_CFG_PMNDS   | MASK_MMSC_CFG_CCTLCSR
+                         | MASK_MMSC_CFG_VCCTL   | MASK_MMSC_CFG_NOPMC
+                         | MASK_MMSC_CFG_EDSP    | MASK_MMSC_CFG_PPMA
+                         | MASK_MMSC_CFG_BF16CVT | MASK_MMSC_CFG_ZFH
+                         | MASK_MMSC_CFG_VL4     | MASK_MMSC_CFG_VECCFG
+                         | MASK_MMSC_CFG_VSIH    | MASK_MMSC_CFG_VDOT
+                         | MASK_MMSC_CFG_VPFH    | MASK_MMSC_CFG_L2CMP_CFG
+                         | MASK_MMSC_CFG_L2C     | MASK_MMSC_CFG_IOCP
+                         | MASK_MMSC_CFG_CORE_PCLUS
+                         | MASK_MMSC_CFG_RVARCH;
 
-    *val = env->andes_csr.csrno[CSR_MMSC_CFG] & mask;
+    *val = env->andes_csr.csrno[CSR_MMSC_CFG] & (target_ulong)mask;
     return RISCV_EXCP_NONE;
 }
 
 void andes_spec_csr_init_nx45v(AndesCsr *andes_csr)
 {
-    andes_csr->csrno[CSR_MMSC_CFG] = MASK_MMSC_CFG_ECD   | MASK_MMSC_CFG_PFT
-                                   | MASK_MMSC_CFG_HSP   | MASK_MMSC_CFG_ACE
-                                   | MASK_MMSC_CFG_VPLIC | MASK_MMSC_CFG_EV5PE
-                                   | MASK_MMSC_CFG_PMNDS
-                                   | MASK_MMSC_CFG_CCTLCSR
-                                   | MASK_MMSC_CFG_PPMA  | MASK_MMSC_CFG_ZFH
-                                   | MASK_MMSC_CFG_VL4
-                                   | MASK_MMSC_CFG_VECCFG;
+    uint64_t mmsc_init_val = MASK_MMSC_CFG_ECD   | MASK_MMSC_CFG_PFT
+                           | MASK_MMSC_CFG_HSP   | MASK_MMSC_CFG_ACE
+                           | MASK_MMSC_CFG_VPLIC | MASK_MMSC_CFG_EV5PE
+                           | MASK_MMSC_CFG_PMNDS
+                           | MASK_MMSC_CFG_CCTLCSR
+                           | MASK_MMSC_CFG_PPMA  | MASK_MMSC_CFG_ZFH
+                           | MASK_MMSC_CFG_VL4
+                           | MASK_MMSC_CFG_VECCFG;
+    andes_csr->csrno[CSR_MMSC_CFG] = (target_ulong)mmsc_init_val;
 
     andes_csr_ops[CSR_MMSC_CFG].read = read_mmsc_cfg_nx45v;
 }
